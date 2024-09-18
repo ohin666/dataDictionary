00039d1445e7c51006175fda938a0b96a38132fe
Owen <owen.6@live.com>
2024-09-18 11:48:47 +0100

    git updates

--- settingRGitHubRepo.R
+++ settingRGitHubRepo.R
@@ -3,7 +3,7 @@
 
 require(usethis)
 
-# create githubtoken to communicate to Rwith GitHub
+# create githubtoken to communicate to R with GitHub
 #* This will take you to a website to set up a token
 #* Copy the token given in the website. This will be used in the next function
 create_github_token()
@@ -11,3 +11,4 @@ create_github_token()
 # Run this function and paste token
 #* this will be in console 
 gitcreds::gitcreds_set()
+
