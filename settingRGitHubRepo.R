fc206a0a9d6317eeaea8af38216b269f8439bef0
Owen <owen.6@live.com>
2024-09-18 11:37:10 +0100

    instructions for Github tokenisation

---
+++ settingRGitHubRepo.R
@@ -0,0 +1,13 @@
+
+# install packages --------------------------------------------------------
+
+require(usethis)
+
+# create githubtoken to communicate to Rwith GitHub
+#* This will take you to a website to set up a token
+#* Copy the token given in the website. This will be used in the next function
+create_github_token()
+
+# Run this function and paste token
+#* this will be in console 
+gitcreds::gitcreds_set()
